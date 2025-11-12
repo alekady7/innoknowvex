d22719ca47f80e0174df26adaa85ed19fbc034f3
Aditya Uniyal <uniyal2001@gmail.com>
2025-11-12 23:46:54 +0530

    render update for user - admin

--- innoknowvex_site/wsgi.py
+++ innoknowvex_site/wsgi.py
@@ -9,8 +9,33 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 
+from django.contrib.auth import get_user_model
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'innoknowvex_site.settings')
 
 application = get_wsgi_application()
+
+def create_admin_on_startup():
+    try:
+        from django.db.utils import OperationalError
+        User = get_user_model()
+        username = os.environ.get("ADMIN_USER", "admin")
+        email = os.environ.get("ADMIN_EMAIL", "admin@example.com")
+        password = os.environ.get("ADMIN_PW")
+        if password:
+            user, created = User.objects.get_or_create(username=username, defaults={"email": email})
+            user.email = email
+            user.is_staff = True
+            user.is_superuser = True
+            user.set_password(password)
+            user.save()
+    except OperationalError:
+        # DB not ready (migrations not run) - ignore
+        pass
+    except Exception:
+        # swallow to avoid bringing down the app; monitor logs
+        import logging
+        logging.exception("create_admin_on_startup failed")
+
+create_admin_on_startup()
